718181c8b2dfff4d53d4d66f9a9a6b92d72b779a
ahmetkrkyn0 <147752769+ahmetkrkyn0@users.noreply.github.com>
2026-03-22 13:47:57 +0300

    frontend bug fixing

--- person-management-hw/frontend/src/App.js
+++ person-management-hw/frontend/src/App.js
@@ -1,15 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FormPage from "./pages/FormPage";
-import ListPage from "./pages/ListPage";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<FormPage />} />
-        <Route path="/people" element={<ListPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
