3e5a755f52f3f4bc2fb8e86785d3a1f439ea3fb9
ahmetkrkyn0 <147752769+ahmetkrkyn0@users.noreply.github.com>
2026-03-22 13:33:39 +0300

    Update index.js

--- person-management-hw/backend/src/index.js
+++ person-management-hw/backend/src/index.js
@@ -55,3 +55,25 @@ app.get("/api/people", async (req, res) => {
     res.status(500).json({ error: "SERVER_ERROR" });
   }
 });
+
+// DELETE
+app.delete("/api/people/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM people WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "NOT_FOUND" });
+    }
+
+    res.json({ message: "Deleted successfully" });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "SERVER_ERROR" });
+  }
+});
